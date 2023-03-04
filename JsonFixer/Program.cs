b004faef70b6e1ae5c492fdf10cc048fdcff5dab
John Somers <j_somers89@yahoo.com>
2023-03-04 09:30:51 -0500

    Add project files.

---
+++ JsonFixer/Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System.Runtime;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace JsonFixer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter bad ID:");
+            string id = Console.ReadLine();
+            string path = System.AppDomain.CurrentDomain.BaseDirectory + "Vault";
+            List<string> fileEntries = Directory.GetFiles(path).ToList();
+            foreach(string fileEntry in fileEntries)
+            {
+                string json = File.ReadAllText(fileEntry);
+                ValutFile valutFile = JsonConvert.DeserializeObject<ValutFile>(json); 
+                // Loop through all objects
+                foreach(ValutFile.Object o in valutFile.Objects)
+                {
+                    int index = 0;
+                    // Loop through all elements
+                    List<ValutFile.Element> elements = new List<ValutFile.Element>();
+                    foreach(ValutFile.Element element in o.Elements)
+                    {
+                        if (element.ObjectID != id)
+                        {
+                            element.Index = index;
+                            elements.Add(element);
+                            index++;
+                        }
+                    }
+                    o.Elements = elements;
+                }
+                string fileName = fileEntry.Replace(".json","FIXED.json");
+                string jsonFixed = JsonConvert.SerializeObject(valutFile, Formatting.Indented);
+                File.WriteAllText(fileName, jsonFixed);
+            }
+        }
+    }
+}
---
+++ JsonFixer/ValutFile.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonFixer
+{
+    public class ValutFile
+    {
+
+       
+            public string FileName { get; set; }
+            public string ReferencePath { get; set; }
+            public string Creator { get; set; }
+            public object[] ModsUsed { get; set; }
+            public List<Object> Objects { get; set; }
+            public string QuickbeltLayoutName { get; set; }
+        
+
+        public class Object
+        {
+            public int Index { get; set; }
+            public int IsContainedIn { get; set; }
+            public int QuickbeltSlotIndex { get; set; }
+            public int InSlotOfRootObjectIndex { get; set; }
+            public int InSlotOfElementIndex { get; set; }
+            public List<Element> Elements { get; set; }
+        }
+
+        public class Element
+        {
+            public int Index { get; set; }
+            public string ObjectID { get; set; }
+            public string Type { get; set; }
+            public Posoffset PosOffset { get; set; }
+            public Orientationforward OrientationForward { get; set; }
+            public Orientationup OrientationUp { get; set; }
+            public int ObjectAttachedTo { get; set; }
+            public int MountAttachedTo { get; set; }
+            public object[] LoadedRoundsInChambers { get; set; }
+            public object[] LoadedRoundsInMag { get; set; }
+            public object[] GenericInts { get; set; }
+            public object[] GenericStrings { get; set; }
+            public object[] GenericVector3s { get; set; }
+            public object[] GenericRotations { get; set; }
+            public Flags Flags { get; set; }
+        }
+
+        public class Posoffset
+        {
+            public float x { get; set; }
+            public float y { get; set; }
+            public float z { get; set; }
+        }
+
+        public class Orientationforward
+        {
+            public float x { get; set; }
+            public float y { get; set; }
+            public float z { get; set; }
+        }
+
+        public class Orientationup
+        {
+            public float x { get; set; }
+            public float y { get; set; }
+            public float z { get; set; }
+        }
+
+        public class Flags
+        {
+            public string[] _keys { get; set; }
+            public string[] _values { get; set; }
+        }
+
+    }
+}
